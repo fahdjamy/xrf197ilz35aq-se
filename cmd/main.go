d6e81400e9f90e4b96826b2b5d601c158d90f44d
fahdjamy <fahdjamy2@gmail.com>
2025-06-29 17:58:11 -0500

    Start the http server in the application's main.go file

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,63 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"errors"
+	"log"
+	"net/http"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
+	"time"
 	"xrf197ilz35aq/internal"
+	xrfHttp "xrf197ilz35aq/internal/server/http"
 )
 
 func main() {
 	env := getAppEnv()
 	config, err := internal.GetConfig(strings.ToLower(env))
 	if err != nil {
-		fmt.Println("failed to load config:", err)
-		return
+		log.Fatalf("failed to load config: %v", err)
 	}
 
 	logger, err := internal.SetupLogger(env, config.Log)
 	if err != nil {
-		fmt.Printf("Failed to setup logger: %v\n", err)
-		return
+		log.Fatalf("Failed to setup logger: %v", err)
 	}
 
+	server := xrfHttp.CreateServer(*logger, config.Application)
+	go func() {
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logger.Error("serverStarted=false :: error starting http server", "error", err)
+		}
+	}()
+
 	logger.Info("***** xrf197ilz35aq started successfully *******")
+
+	ch := make(chan os.Signal, 1)
+	// Accept graceful shutdowns when quit via SIGINT (Ctrl+C), SIGTERM (used by Docker, Kubernetes) or SIGQUIT.
+	// SIGKILL will not be caught.
+	signal.Notify(ch, os.Interrupt)
+	signal.Notify(ch, syscall.SIGTERM)
+	signal.Notify(ch, syscall.SIGQUIT)
+
+	// Block until we receive shutdown signal.
+	<-ch
+	logger.Info("received shutdown signal, starting graceful shutdown")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	// Attempt to gracefully shut down the server.
+	err = server.Shutdown(ctx)
+	if err != nil {
+		logger.Error("xrf197ilz35aq shutdown failed", "error", err)
+		os.Exit(1)
+	}
+
+	logger.Info("xrf197ilz35aq shutdown successfully")
+	os.Exit(0)
 }
 
 func getAppEnv() string {
