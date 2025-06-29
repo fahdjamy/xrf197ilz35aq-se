53e72304f07187017a7d5ef351130d291c9a3dee
fahdjamy <fahdjamy2@gmail.com>
2025-06-29 16:29:21 -0500

    Add a logger file to setup slog logger and initial dev configurations.

---
+++ internal/logger.go
@@ -0,0 +1,55 @@
+package internal
+
+import (
+	"fmt"
+	"github.com/lmittmann/tint"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"io"
+	"log/slog"
+	"os"
+	"strings"
+	"sync"
+)
+
+var (
+	lock    = &sync.Mutex{}
+	sLogger *slog.Logger
+)
+
+func SetupLogger(env string, config LogConfig) (*slog.Logger, error) {
+	lock.Lock()
+	defer lock.Unlock()
+
+	var logLevel = new(slog.LevelVar)
+	opts := slog.HandlerOptions{Level: logLevel, AddSource: true}
+
+	if strings.ToUpper(env) == ProductionEnv {
+		fileOutput := config.OutputFile
+		logFileWriter := &lumberjack.Logger{
+			Filename:   fileOutput,
+			MaxSize:    100, // megabytes
+			MaxBackups: 5,
+			MaxAge:     10, // days
+			LocalTime:  true,
+			Compress:   true,
+		}
+
+		logLevel.Set(slog.LevelInfo)
+
+		wo := io.MultiWriter(os.Stdout, logFileWriter)
+		multiWriter := slog.NewTextHandler(wo, &opts)
+
+		sLogger = slog.New(multiWriter)
+		sLogger.Info("logger setup", "logLevel", "INFO", "logOutputs", "console and file")
+		return sLogger, nil
+	}
+
+	logLevel.Set(slog.LevelDebug) // "dev", "test" or any other environment
+	sLogger = slog.New(tint.NewHandler(os.Stdout, &tint.Options{
+		Level:       opts.Level,
+		AddSource:   opts.AddSource,
+		ReplaceAttr: opts.ReplaceAttr,
+	}))
+	sLogger.Debug(fmt.Sprintf("running in %s mode", env), "logLevel", "DEBUG", "logOutput", "console only")
+	return sLogger, nil
+}
