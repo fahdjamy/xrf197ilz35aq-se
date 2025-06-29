3996ff324932cd434fc6d778f8d249fe875e589b
fahdjamy <fahdjamy2@gmail.com>
2025-06-29 17:26:12 -0500

    Setup http request server

---
+++ internal/server/http/server.go
@@ -0,0 +1,33 @@
+package http
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+	"time"
+	"xrf197ilz35aq/internal"
+	"xrf197ilz35aq/internal/server/http/handlers"
+)
+
+func CreateServer(logger slog.Logger, appConfig internal.AppConfig) *http.Server {
+	serverMux := http.NewServeMux()
+
+	reqHandlers := make([]handlers.RequestHandler, 0)
+
+	// create request (routes) handlers
+	healthReqHandler := handlers.NewReqHealthHandlers(logger)
+
+	reqHandlers = append(reqHandlers, healthReqHandler)
+
+	for _, handler := range reqHandlers {
+		handler.RegisterRoutes(serverMux)
+	}
+
+	return &http.Server{
+		Handler:      serverMux,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 16 * time.Minute,
+		IdleTimeout:  16 * time.Minute,
+		Addr:         fmt.Sprintf(":%d", appConfig.Port),
+	}
+}
