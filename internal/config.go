077de304db9ae6970e58fb9a58c532c41611c668
fahdjamy <fahdjamy2@gmail.com>
2025-06-29 16:21:12 -0500

    Setup config loader for the application

---
+++ internal/config.go
@@ -0,0 +1,79 @@
+package internal
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"strings"
+	"sync"
+)
+
+type LogConfig struct {
+	OutputFile string `yaml:"outputFile"`
+}
+
+type RedisConfig struct {
+	Address      string `yaml:"address"`
+	Password     string `yaml:"password"`
+	Database     int    `yaml:"database"`
+	Protocol     int    `yaml:"protocol"`
+	PoolSize     int    `yaml:"poolSize"`
+	MaxRetries   int    `yaml:"maxRetries"`
+	DialTimeout  int    `yaml:"dialTimeout"`
+	ReadTimeout  int    `yaml:"readTimeout"`
+	MinIdleConns int    `yaml:"minIdleConns"`
+	WriteTimeout int    `yaml:"writeTimeout"`
+}
+
+type Config struct {
+	Log   LogConfig   `yml:"log"`
+	Redis RedisConfig `yml:"redis"`
+}
+
+var (
+	config     *Config
+	configOnce sync.Once
+	configErr  error
+)
+
+func loadConfigs(env string) (*Config, error) {
+	configOnce.Do(func() {
+		configFilePath := fmt.Sprintf("config-%s.yaml", env)
+		viper.SetConfigName(configFilePath)
+		viper.AddConfigPath("./configs")
+		viper.SetConfigType("yaml")
+
+		// AutomaticEnv check for an environment variable any time a viper.Get request is made.
+
+		// Rules: viper checks for an environment variable w/ a name matching the key uppercased and prefixed with the EnvPrefix if set.
+		viper.AutomaticEnv()
+		viper.SetEnvPrefix("XRF_Q2") // will be uppercased automatically
+		// this is useful e.g., want to use . in Get() calls, but environmental variables are to use _ delimiters (e.g., app.port -> APP_PORT)
+		viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+		// Read the config file
+		err := viper.ReadInConfig()
+		if err != nil {
+			configErr = fmt.Errorf("failed to read config file: %w :: env=%s", err, env)
+			return
+		}
+
+		appConfig := Config{}
+		err = viper.Unmarshal(&appConfig)
+		if err != nil {
+			configErr = fmt.Errorf("failed to unmarshal config file: %w :: env=%s", err, env)
+			return
+		}
+
+		config = &appConfig
+	})
+
+	// Important: Check the global error variable *after* once.Do.
+	if configErr != nil {
+		return nil, configErr // Return the stored error
+	}
+	return config, nil
+}
+
+func GetConfig(env string) (*Config, error) {
+	return loadConfigs(env)
+}
