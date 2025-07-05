62690c754b3b207948716a0bab324d3e6b61578b
fahdjamy <fahdjamy2@gmail.com>
2025-07-05 13:17:33 -0500

    Add a user processor to process user request to create a new user

---
+++ internal/processor/user.go
@@ -0,0 +1,32 @@
+package processor
+
+import (
+	"context"
+	"log/slog"
+	"time"
+	"xrf197ilz35aq/internal/client"
+	"xrf197ilz35aq/internal/model"
+)
+
+type UserProcessor struct {
+	log       slog.Logger
+	apiClient client.ApiClient
+}
+
+func (up *UserProcessor) CreateUser(ctx context.Context, userReq model.UserRequest) (*model.UserResponse, error) {
+	// 1. Validate user request
+	if err := userReq.Validate(); err != nil {
+		return nil, err
+	}
+
+	// 2. Make request to create user
+	userResp := &model.UserResponse{}
+	timeoutCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	err := up.apiClient.Post(timeoutCtx, "/user", userReq, nil, &userResp)
+	if err != nil {
+		return nil, err
+	}
+
+	return userResp, nil
+}
