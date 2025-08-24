b9f8f32c6cf2168f8f6bc71d1f4f6186ebfd2ee0
fahdjamy <fahdjamy2@gmail.com>
2025-08-23 23:59:00 -0500

    Refactor methods for adding headers to gRPC request

--- internal/processor/account.go
+++ internal/processor/account.go
@@ -5,17 +5,14 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-	"time"
 	v1 "xrf197ilz35aq/gen/account/v1"
 	"xrf197ilz35aq/internal"
 	"xrf197ilz35aq/internal/model"
-
-	"google.golang.org/grpc/metadata"
-	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type AccountProcessor interface {
 	CreateAccount(ctx context.Context, userCtx model.UserContext, req model.AccountRequest) (model.AccountResponse, error)
+	FindAccounts(ctx context.Context, userCtx model.UserContext, req model.FindAccountRequest) ([]model.AccountResponse, error)
 }
 
 type accountProcessor struct {
@@ -33,13 +30,7 @@ func (ap *accountProcessor) CreateAccount(
 		}
 	}
 
-	// Create gRPC metadata (headers).
-	md := metadata.New(map[string]string{
-		internal.XrfUserFingerPrint: userCtx.Fingerprint,
-	})
-
-	// Create a new context with the metadata attached.
-	gRPCCtxWithHeaders := metadata.NewOutgoingContext(ctx, md)
+	gRPCCtxWithHeaders := createGrpcContextWithHeaders(ctx, userCtx)
 
 	// make a call to the gRPC service to create an account
 	resp, err := ap.grpcAcctClient.CreateAccount(gRPCCtxWithHeaders, &v1.CreateAccountRequest{
@@ -51,18 +42,18 @@ func (ap *accountProcessor) CreateAccount(
 		return model.AccountResponse{}, fmt.Errorf("failed to create account: %w", err)
 	}
 
-	createdAt, err := ConvertTimestamp(resp.Account.CreationTime, req.Timezone)
-	if err := CheckConvertedGrpcTimeErr(err); err != nil {
+	createdAt, err := convertTimestamp(resp.Account.CreationTime, req.Timezone)
+	if err := checkConvertedGrpcTimeErr(err); err != nil {
 		return model.AccountResponse{}, err
 	}
-	modifiedAt, err := ConvertTimestamp(resp.Account.ModificationTime, req.Timezone)
-	if err := CheckConvertedGrpcTimeErr(err); err != nil {
+	modifiedAt, err := convertTimestamp(resp.Account.ModificationTime, req.Timezone)
+	if err := checkConvertedGrpcTimeErr(err); err != nil {
 		return model.AccountResponse{}, err
 	}
 
 	walletHoldingResp := resp.Account.WalletHolding
-	walletModificationTime, err := ConvertTimestamp(walletHoldingResp.ModificationTime, req.Timezone)
-	if err := CheckConvertedGrpcTimeErr(err); err != nil {
+	walletModificationTime, err := convertTimestamp(walletHoldingResp.ModificationTime, req.Timezone)
+	if err := checkConvertedGrpcTimeErr(err); err != nil {
 		return model.AccountResponse{}, err
 	}
 
@@ -81,42 +72,16 @@ func (ap *accountProcessor) CreateAccount(
 	}, nil
 }
 
-// ConvertTimestamp dynamically converts a protobuf timestamp to a Go time.Time
-// in the specified timezone.
-func ConvertTimestamp(ts *timestamppb.Timestamp, tz string) (time.Time, error) {
-	// check if the timestamp is valid.
-	if err := ts.CheckValid(); err != nil {
-		return time.Time{}, internal.ErrInvalidGRPCTimeStamp
-	}
-
-	// Load the specified location/timezone.
-	location, err := time.LoadLocation(tz)
-	if err != nil {
-		return time.Time{}, fmt.Errorf("could not load location '%s': %w", tz, err)
-	}
-
-	// Convert the protobuf timestamp to a Go time.Time object.
-	// The AsTime() method returns the time in UTC.
-	goTime := ts.AsTime()
-
-	// Return the time in the desired location.
-	return goTime.In(location), nil
-}
-
-func CheckConvertedGrpcTimeErr(err error) error {
-	if err != nil {
-		if errors.Is(err, internal.ErrInvalidGRPCTimeStamp) {
-			return &internal.ServerError{
-				Message: "something went wrong",
-				Err:     err,
-			}
-		}
-		return &internal.ExternalError{
+func (ap *accountProcessor) FindAccounts(ctx context.Context, userCtx model.UserContext,
+	req model.FindAccountRequest) ([]model.AccountResponse, error) {
+	if err := req.Validate(); err != nil {
+		return []model.AccountResponse{}, &internal.ExternalError{
 			Message: err.Error(),
-			Code:    400,
+			Code:    http.StatusBadRequest,
 		}
 	}
-	return nil
+
+	return nil, errors.New("not implemented")
 }
 
 func NewAccountProcessor(grpcAcctService v1.AccountServiceClient) AccountProcessor {
--- internal/processor/processor.go
+++ internal/processor/processor.go
@@ -1,7 +1,68 @@
 package processor
 
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+	"xrf197ilz35aq/internal"
+	"xrf197ilz35aq/internal/model"
+
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
 type Processors struct {
 	UserProcessor    UserProcessor
 	AuthProcessor    AuthProcessor
 	AccountProcessor AccountProcessor
+}
+
+// convertTimestamp dynamically converts a protobuf timestamp to a Go time.Time
+// in the specified timezone.
+func convertTimestamp(ts *timestamppb.Timestamp, tz string) (time.Time, error) {
+	// check if the timestamp is valid.
+	if err := ts.CheckValid(); err != nil {
+		return time.Time{}, internal.ErrInvalidGRPCTimeStamp
+	}
+
+	// Load the specified location/timezone.
+	location, err := time.LoadLocation(tz)
+	if err != nil {
+		return time.Time{}, fmt.Errorf("could not load location '%s': %w", tz, err)
+	}
+
+	// Convert the protobuf timestamp to a Go time.Time object.
+	// The AsTime() method returns the time in UTC.
+	goTime := ts.AsTime()
+
+	// Return the time in the desired location.
+	return goTime.In(location), nil
+}
+
+func checkConvertedGrpcTimeErr(err error) error {
+	if err != nil {
+		if errors.Is(err, internal.ErrInvalidGRPCTimeStamp) {
+			return &internal.ServerError{
+				Message: "something went wrong",
+				Err:     err,
+			}
+		}
+		return &internal.ExternalError{
+			Message: err.Error(),
+			Code:    400,
+		}
+	}
+	return nil
+}
+
+func createGrpcContextWithHeaders(ctx context.Context, userCtx model.UserContext) context.Context {
+	// Create gRPC metadata (headers).
+	md := metadata.New(map[string]string{
+		internal.XrfUserFingerPrint: userCtx.Fingerprint,
+	})
+
+	// Create a new context with the metadata attached.
+	gRPCCtxWithHeaders := metadata.NewOutgoingContext(ctx, md)
+	return gRPCCtxWithHeaders
 }
