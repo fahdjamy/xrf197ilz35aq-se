aa9d4303f912aa8f04b43881fba08308d4e98e55
fahdjamy <fahdjamy2@gmail.com>
2025-11-01 09:54:18 -0500

    Bump deps

--- internal/processor/asset.go
+++ internal/processor/asset.go
@@ -2,14 +2,19 @@ package processor
 
 import (
 	"context"
+	v1 "xrf197ilz35aq/gen/xrfq1/asset/v1"
 	"xrf197ilz35aq/internal/model"
+	"xrf197ilz35aq/internal/service"
 )
 
 type AssetProcessor interface {
 	CreateAsset(ctx context.Context, userCtx model.UserContext, req model.AssetRequest) (bool, error)
 }
 
-type assetProcessor struct{}
+type assetProcessor struct {
+	grpcAcctClient v1.AssetServiceClient
+	orgService     service.OrgService
+}
 
 func (assetProcessor) CreateAsset(ctx context.Context, userCtx model.UserContext, req model.AssetRequest) (bool, error) {
 	return true, nil
