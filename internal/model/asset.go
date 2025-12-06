f356f3032eb4b6166bdcd089906fdad47f03a7ce
fahdjamy <fahdjamy2@gmail.com>
2025-12-06 14:27:56 -0600

    Add method to validate an asset's name

--- internal/model/asset.go
+++ internal/model/asset.go
@@ -1,6 +1,20 @@
 package model
 
+import (
+	"fmt"
+)
+
 type AssetRequest struct {
 	Name  string `json:"name"`
 	OrgId string `json:"orgId"`
+}
+
+func (req *AssetRequest) Validate() error {
+	if req.OrgId == "" {
+		return fmt.Errorf("invalid orgId")
+	}
+	if req.Name == "" || len(req.Name) < 2 || len(req.Name) > 32 {
+		return fmt.Errorf("invalid org name")
+	}
+	return nil
 }
