97299f92234e18601c5e786beaae442673189505
fahdjamy <fahdjamy2@gmail.com>
2025-12-07 14:13:35 -0600

    Implement method to create a new asset

--- internal/processor/asset.go
+++ internal/processor/asset.go
@@ -2,7 +2,9 @@ package processor
 
 import (
 	"context"
+	"net/http"
 	v1 "xrf197ilz35aq/gen/xrfq1/asset/v1"
+	"xrf197ilz35aq/internal"
 	"xrf197ilz35aq/internal/model"
 	"xrf197ilz35aq/internal/service"
 )
@@ -16,7 +18,26 @@ type assetProcessor struct {
 	orgService     service.OrgService
 }
 
-func (assetProcessor) CreateAsset(ctx context.Context, userCtx model.UserContext, req model.AssetRequest) (bool, error) {
+func (ap *assetProcessor) CreateAsset(ctx context.Context, userCtx model.UserContext, req model.AssetRequest) (bool, error) {
+	if err := req.Validate(); err != nil {
+		return false, &internal.ExternalError{
+			Message: err.Error(),
+			Code:    http.StatusBadRequest,
+		}
+	}
+
+	gRPCCtxWithHeaders := createGrpcContextWithHeaders(ctx, userCtx)
+	_, err := ap.grpcAcctClient.Create(gRPCCtxWithHeaders, &v1.CreateRequest{
+		Name:         "",
+		Symbol:       "",
+		Description:  "",
+		Organization: "",
+	})
+
+	if err != nil {
+		return false, handleGrpcError(err)
+	}
+
 	return true, nil
 }
 
